f073a0855a2bfa77341b49833e80b03e5f1de414
Ali Naqvi <syed.alinaqvi@gmail.com>
2021-03-24 23:36:20 +0800

    Updated for Crystal 1.0

--- src/fnv.cr
+++ src/fnv.cr
@@ -1,7 +1,7 @@
 # FNV module implements FNV-1 and FNV-1a, non-cryptographic hash functions created by
 # Glenn Fowler, Landon Curt Noll, and Phong Vo.
 module FNV
-  VERSION = "0.1.1"
+  VERSION = "0.1.3"
 end
 
 require "./fnv/**"
